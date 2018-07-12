fbd0ab63e261f94ba6dddbf7b18344b4d315c981
Nico <chazeau.nicolas63@gmail.com>
2018-07-12 16:33:43 +0200

    ajout multiplication

--- src/com/human/booster/calculator/Calcultor.java
+++ src/com/human/booster/calculator/Calcultor.java
@@ -5,6 +5,8 @@ public class Calcultor {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(new CalculatorController().addition(2,3));
+		System.out.println(new CalculatorController().multiplication(2,3));
+
  //test
 	}
 
