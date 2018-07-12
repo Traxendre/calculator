1b53e0f7e3f34dd6cda51bf56f6512e997afae02
Jeremie <jpiris@test.com>
2018-07-12 15:58:46 +0200

    Substraction

--- src/com/human/booster/calculator/Calcultor.java
+++ src/com/human/booster/calculator/Calcultor.java
@@ -5,6 +5,24 @@ public class Calcultor {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
  //test
+
+		//ADDITION
+		//SOUSTRACTION
+		//DIVISION
+		//MULTIPLICATION
+
+		public double soustraction (){
+			double a = new double[];
+			double b = new double[];
+
+			double c = a - b ;
+
+		}
+
+
+		public double division (){
+
+		}
 	}
 
 }
