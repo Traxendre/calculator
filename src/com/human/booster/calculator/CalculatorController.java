2c60fe643be537d419e8dcfbdb7cb76102293f1f
K <k@test.fr>
2018-07-12 15:53:08 +0200

    code

---
+++ src/com/human/booster/calculator/CalculatorController.java
@@ -0,0 +1,20 @@
+package com.human.booster.calculator;
+
+public class CalculatorController {
+
+	public double addition(double num1, double num2) {
+		return 0;
+	}
+
+	public double subtraction(double num1, double num2) {
+		return 0;
+	}
+
+	public double multiplication(double num1, double num2) {
+		return 0;
+	}
+
+	public double division(double num1, double num2) {
+		return 0;
+	}
+}
