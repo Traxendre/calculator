60c099b4c57f5f733718a54e20c8d3aff604df19
ben <b@b.fr>
2018-07-12 16:21:57 +0200

    test addition

--- src/com/human/booster/calculator/Calcultor.java
+++ src/com/human/booster/calculator/Calcultor.java
@@ -4,6 +4,7 @@ public class Calcultor {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println(new CalculatorController().addition(2,3));
  //test
 	}
 
