80ed7d42897d1bf7e26bc961448119d286297703
atom <atom@fortytwo.fr>
2018-07-12 15:59:15 +0200

    great team effort to offer multiplication to our beloved client.
    xxxx

--- src/com/human/booster/calculator/CalculatorController.java
+++ src/com/human/booster/calculator/CalculatorController.java
@@ -11,7 +11,7 @@ public class CalculatorController {
 	}
 
 	public double multiplication(double num1, double num2) {
-		return 0;
+		return num1 * num2;
 	}
 
 	public double division(double num1, double num2) {
