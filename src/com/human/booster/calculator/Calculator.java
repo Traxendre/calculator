917c7043700f19995d99fd4d0d370b53b2012757
atom <atom@fortytwo.fr>
2018-07-13 08:48:05 +0200

    clean

--- src/com/human/booster/calculator/Calculator.java
+++ src/com/human/booster/calculator/Calculator.java
@@ -1,13 +1,12 @@
 package com.human.booster.calculator;
 
-public class Calcultor {
+public class Calculator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println(new CalculatorController().addition(2,3));
 		System.out.println(new CalculatorController().multiplication(2,3));
-System.out.println(new CalculatorController().division(2,3));
-
+		System.out.println(new CalculatorController().division(2,3));
+		System.out.println(CalculatorController.subtraction(2,3));
 	}
 
 }
